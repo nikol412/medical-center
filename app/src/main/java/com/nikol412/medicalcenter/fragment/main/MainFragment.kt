6b4353a4fe66d5e99d8b6cb39b0f2add612bbff6
nikol412 <travin.nikita@gmail.com>
2021-05-20 23:06:25 +0600

    add navigation file, main fragment

---
+++ app/src/main/java/com/nikol412/medicalcenter/fragment/main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.nikol412.medicalcenter.fragment.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.nikol412.medicalcenter.R
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
