628f5d687731039d377ec5a2e46a00890d8242da
nikol412 <travin.nikita@gmail.com>
2021-05-20 23:05:52 +0600

    add test login screen

--- app/src/main/java/com/nikol412/medicalcenter/fragment/LoginFragment.kt
+++ app/src/main/java/com/nikol412/medicalcenter/fragment/LoginFragment.kt
@@ -1,32 +1,33 @@
 package com.nikol412.medicalcenter.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.nikol412.medicalcenter.R
+import com.nikol412.medicalcenter.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = LoginFragment()
+    private val viewModel: LoginViewModel by viewModels()
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel.navController = findNavController()
     }
-
-    private lateinit var viewModel: LoginViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        binding.vm = viewModel
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        return binding.root
     }
 
 }
--- app/src/main/java/com/nikol412/medicalcenter/fragment/LoginViewModel.kt
+++ app/src/main/java/com/nikol412/medicalcenter/fragment/LoginViewModel.kt
@@ -1,7 +1,24 @@
 package com.nikol412.medicalcenter.fragment
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.nikol412.medicalcenter.R
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var navController: NavController? = null
+
+    private val login = "Nika"
+    private val password = "Nika"
+    val userLogin = MutableLiveData("")
+    val userPassword = MutableLiveData("")
+
+
+
+    fun onLoginClick() {
+        if (userLogin.value == login && userPassword.value == password) {
+            navController?.navigate(R.id.mainFragment)
+        }
+    }
 }
