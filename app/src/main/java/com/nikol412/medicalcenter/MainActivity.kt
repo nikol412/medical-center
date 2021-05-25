fb6ed6383267e190dee2ed35427486bf4fca581a
nikol412 <travin.nikita@gmail.com>
2021-05-25 13:42:35 +0600

    add toolbar, fragment titles

--- app/src/main/java/com/nikol412/medicalcenter/MainActivity.kt
+++ app/src/main/java/com/nikol412/medicalcenter/MainActivity.kt
@@ -2,10 +2,23 @@ package com.nikol412.medicalcenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
